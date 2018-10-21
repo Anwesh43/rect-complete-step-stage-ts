8e21fd9465ebe64a5150868e9981b429e8251b13
Anwesh43 <anweshthecool0@gmail.com>
2018-10-21 14:59:14 +0530

    adding RCSNode class

--- RectCompleteStepStage.ts
+++ RectCompleteStepStage.ts
@@ -72,3 +72,67 @@ class Animator {
         }
     }
 }
+
+class RCSNode {
+    prev : RCSNode
+    next : RCSNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new RCSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap = w / (nodes + 1)
+        const size = 2 * gap / 3
+        const hSize = size / 3
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / 60
+        context.strokeStyle = 'white'
+        context.save()
+        context.translate(this.i * gap + gap, h/2)
+        for (var j = 0; j < 2; j++) {
+            const sc : number = Math.min(0.5, Math.max(0, this.state.scale - 0.5 * j)) * 2
+            const wu = size * sc, hu = hSize * sc
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            context.beginPath()
+            context.moveTo(-size/2, -size/2)
+            context.lineTo(-size/2 + wu, -size/2)
+            context.stroke()
+            context.beginPath()
+            context.moveTo(-size/2, -size/2)
+            context.lineTo(-size/2, -size/2 + hu)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RCSNode {
+        var curr : RCSNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
